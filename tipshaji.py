d59d64b9b840aef077b699baa2403eabce802a0e
Budi Mulyono <budi.mulyono@gmail.com>
2025-01-11 14:33:08 +0700

    change to markdown

--- tipshaji.py
+++ tipshaji.py
@@ -10,9 +10,7 @@ st.title("Perlengkapan Haji")
 def load_data(nrows):
     columns_to_keep = ["Nama Produk", "Harga", "Link Komisi Ekstra"]
     data = pd.read_csv("LinkProdukSekaligus.csv", usecols=columns_to_keep)
-    # data.rename(columns={"Link Komisi Ekstra":"Link Toko"})
     data.columns = ["Nama Produk","Harga","Link Toko"]
-    data.replace(r"\|", " ", regex=True)
     data['Nama Produk'] = data['Nama Produk'].apply(normalize_text)
     print(data)
     return data.to_markdown()
