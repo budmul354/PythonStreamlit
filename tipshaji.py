7af4bc56eef05c144721fb056764d1ccc138013a
Budi Mulyono <budi.mulyono@gmail.com>
2025-01-13 23:48:30 +0700

    change to container and dataframe

--- tipshaji.py
+++ tipshaji.py
@@ -11,9 +11,11 @@ def load_data(nrows):
     columns_to_keep = ["Nama Produk", "Harga", "Link Komisi Ekstra"]
     data = pd.read_csv("LinkProdukSekaligus.csv", usecols=columns_to_keep)
     data.columns = ["Nama Produk","Harga","Link Toko"]
-    data['Nama Produk'] = data['Nama Produk'].apply(normalize_text)
-    print(data)
-    return data.to_markdown()
+    # data['Nama Produk'] = data['Nama Produk'].apply(normalize_text)
+    # print(data)
+    # return data.to_markdown()
+    data.index = range(1, len(data) + 1)
+    return data
 
 def normalize_text(text):
     return re.sub(r"\|"," ",text)
@@ -21,7 +23,10 @@ def normalize_text(text):
 
 def main():
     data = load_data(1)
-    st.markdown(data)
+    # st.markdown(data)
+    container = st.container()
+    with container:
+        st.dataframe(data,column_config={"Link Toko": st.column_config.LinkColumn()}, height=None)
 
 if __name__ == '__main__':
     main()
